48dd36c4a5141c0c0d4eb2f332e25d07a7475b79
Yaakoubi Mohamed <mohamedyaakoubiweb@gmail.com>
2024-03-08 17:26:22 +0100

    Update participantController.js

--- src/controllers/participantController.js
+++ src/controllers/participantController.js
@@ -1,14 +1,62 @@
 const Participant = require('../models/participant')
 
 
-exports.createParticipant = async (req, res) =>{
+// **Liste des participants**
+exports.listParticipants = async (req, res) => {
+  try {
+    const participants = await Participant.find();
+    res.json({ success: true, data: participants });
+  } catch (error) {
+    next(error);
+  }
+};
 
-    const participant =  new Participant(req.body);
-    try{
-        await participant.save();
-        res.json({data: participant})
-    } catch(err){
-        console.log("Error");
-        res.status(400).send({message: err.message});
+// **Afficher un participant**
+exports.getParticipant = async (req, res) => {
+  try {
+    const participant = await Participant.findById(req.params.id);
+    if (!participant) {
+      throw new Error('Participant introuvable');
     }
-}
+    res.json({ success: true, data: participant });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// **Créer un participant**
+exports.createParticipant = async (req, res) => {
+  try {
+    const participant = new Participant(req.body);
+    await participant.save();
+    res.json({ success: true, data: participant });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// **Modifier un participant**
+exports.updateParticipant = async (req, res) => {
+  try {
+    const participant = await Participant.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!participant) {
+      throw new Error('Participant introuvable');
+    }
+    res.json({ success: true, data: participant });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// **Supprimer un participant**
+exports.deleteParticipant = async (req, res) => {
+  try {
+    const participant = await Participant.findByIdAndDelete(req.params.id);
+    if (!participant) {
+      throw new Error('Participant introuvable');
+    }
+    res.json({ success: true, message: 'Participant supprimé avec succès' });
+  } catch (error) {
+    next(error);
+  }
+};
