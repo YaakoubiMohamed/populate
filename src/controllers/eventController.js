b52742155ace1675612d8ca9ee9e60c9d4b23dd9
Yaakoubi Mohamed <mohamedyaakoubiweb@gmail.com>
2024-03-08 17:28:37 +0100

    Update eventController.js

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,20 +1,62 @@
 const Event = require('../models/event')
+const Participant = require('../models/participant')
 
-exports.getEvents =  async (req, res) => {
-    const events = await Event.find()
-    .populate('participants');
+// **Liste des événements**
+exports.listEvenements = async (req, res) => {
+  try {
+    const evenements = await Event.find().populate('participants');
+    res.json({ success: true, data: evenements });
+  } catch (error) {
+    next(error);
+  }
+};
 
-    res.json({events: events});
-}
-
-exports.createEvents  = async (req, res) =>{
-    const event = new Event(req.body);
-
-    try{
-        await event.save();
-        res.json({message: 'Event created!'})
-    }catch(err){
-        console.log(err);
-        res.status(400).json({message:"unable to create event"});
+// **Afficher un événement**
+exports.getEvenement = async (req, res) => {
+  try {
+    const evenement = await Event.findById(req.params.id).populate('participants');
+    if (!evenement) {
+      throw new Error('Evenement introuvable');
     }
-}
+    res.json({ success: true, data: evenement });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// **Créer un événement**
+exports.createEvenement = async (req, res) => {
+  try {
+    const evenement = new Event(req.body);
+    await evenement.save();
+    res.json({ success: true, data: evenement });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// **Modifier un événement**
+exports.updateEvenement = async (req, res) => {
+  try {
+    const evenement = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!evenement) {
+      throw new Error('Evenement introuvable');
+    }
+    res.json({ success: true, data: evenement });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// **Supprimer un événement**
+exports.deleteEvenement = async (req, res) => {
+  try {
+    const evenement = await Event.findByIdAndDelete(req.params.id);
+    if (!evenement) {
+      throw new Error('Evenement introuvable');
+    }
+    res.json({ success: true, message: 'Evenement supprimé avec succès' });
+  } catch (error) {
+    next(error);
+  }
+};
