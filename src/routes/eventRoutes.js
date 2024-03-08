27f55315989f5c582aad58a489ed1da699ce5db3
Yaakoubi Mohamed <mohamedyaakoubiweb@gmail.com>
2024-03-08 17:31:23 +0100

    Update eventRoutes.js

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 
-router.get('/event', eventController.getEvents);
-router.post('/event', eventController.createEvents);
+// **Liste des événements**
+router.get('/', EvenementsController.listEvenements);
+
+// **Afficher un événement**
+router.get('/:id', EvenementsController.getEvenement);
+
+// **Créer un événement**
+router.post('/', EvenementsController.createEvenement);
+
+// **Modifier un événement**
+router.put('/:id', EvenementsController.updateEvenement);
+
+// **Supprimer un événement**
+router.delete('/:id', EvenementsController.deleteEvenement);
 
 module.exports = router;
